5a161db4b50c3ac18bd12813d1dcb8c9259cdc84
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-03-27 11:41:15 +0100

    add creating user doc & initial jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,27 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const User = require("../models/User");
+
 const register = async (req, res) => {
-  res.send("register user");
+  const user = await User.create({ ...req.body });
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      name: user.name,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+
+  res.status(StatusCodes.CREATED).json({
+    user: { name: user.name },
+    token,
+  });
 };
 
 const login = async (req, res) => {
